f214668c753a68822e00d6a42f7ebb665481c8fd
BryanMurkyChan <118821439+BryanMurkyChan@users.noreply.github.com>
2024-10-02 13:38:09 +0800

    Update download.py

--- model/download.py
+++ model/download.py
@@ -1,3 +1,4 @@
 from modelscope import snapshot_download
+import os
 DOWNLOAD_PATH = os.path.join(os.path.dirname(__file__))
 model_dir = snapshot_download("AI-ModelScope/bge-large-zh-v1.5", cache_dir=DOWNLOAD_PATH)
