0ac28486f90482e785670321b8ec359ac14c8b1b
BryanMurkyChan <118821439+BryanMurkyChan@users.noreply.github.com>
2024-10-04 12:59:10 +0800

    Update download.py

--- model/download.py
+++ model/download.py
@@ -1,4 +1,23 @@
-from modelscope import snapshot_download
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
-DOWNLOAD_PATH = os.path.join(os.path.dirname(__file__))
-model_dir = snapshot_download("AI-ModelScope/bge-large-zh-v1.5", cache_dir=DOWNLOAD_PATH)
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"  # 设置为hf的国内镜像网站
+
+from huggingface_hub import snapshot_download
+
+model_name = "infgrad/stella-base-zh-v3-1792d"
+# while True 是为了防止断联
+while True:
+    try:
+        snapshot_download(
+            repo_id=model_name,
+            local_dir_use_symlinks=True,  # 在local-dir指定的目录中都是一些“链接文件”
+            ignore_patterns=["*.bin"],  # 忽略下载哪些文件
+            local_dir=DOWNLOAD_PATH,
+            token="*************",   # huggingface的token
+            resume_download=True        )
+        break
+    except:
+        pass
+
